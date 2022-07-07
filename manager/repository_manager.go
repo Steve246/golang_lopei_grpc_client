6a5d978d3ddb8d45622770622b91bcef8a265583
Steve Jo <tedjasuryastevenjonathan@gmail.com>
2022-07-07 16:29:57 +0700

    addManager

--- manager/repository_manager.go
+++ manager/repository_manager.go
@@ -1,1 +1,20 @@
 package manager
+
+import "golang_lopei_grc_client/repository"
+
+type RepositoryManager interface {
+	CustomerRepo() repository.CustomerRepository
+}
+
+type repositoryManager struct {
+	infraManager infraManager
+}
+
+
+func(r *repositoryManager) CustomerRepo() repository.CustomerRepository {
+	return repository.NewCustomerRepository(r.infraManager.LopeiClientConn())
+}
+
+func NewRepositoryManager(infraManager infraManager) RepositoryManager {
+	return &repositoryManager{infraManager: infraManager}
+}
--- manager/usecase_manager.go
+++ manager/usecase_manager.go
@@ -1,1 +1,21 @@
 package manager
+
+import "golang_lopei_grc_client/usecase"
+
+type UseCaseManager interface {
+	CheckBalanceUsecase() usecase.CheckBalanceUsecase
+}
+
+
+type useCaseManager struct {
+	repoManager RepositoryManager
+}
+
+func (u *useCaseManager) CheckBalanceUsecase() usecase.CheckBalanceUsecase {
+	return usecase.NewCheckBalanceUseCase(u.repoManager.CustomerRepo())
+
+}
+
+func NewUseCaseManager(repoManager RepositoryManager) UseCaseManager {
+	return &useCaseManager{repoManager: repoManager}
+}
